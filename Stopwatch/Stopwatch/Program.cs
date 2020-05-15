99682c51629dc5a0ad07bfb68ce17fe43edb9f9f
Leon <leonardo.held@grad.ufsc.br>
2020-05-15 08:41:36 -0700

    StopWatch from Section 2

---
+++ Stopwatch/Stopwatch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            stopwatch.Stop();
+
+            Console.WriteLine("Elapsed time is: {0}", stopwatch.ElapsedTime());
+
+            stopwatch.Start();
+            stopwatch.Start();
+            System.Threading.Thread.Sleep(1000);
+            stopwatch.Stop();
+            Console.WriteLine("Elapsed time is: {0}", stopwatch.ElapsedTime());
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch/Stopwatch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stopwatch
+{
+    class Stopwatch
+    {
+        private DateTime StartTime;
+
+        private DateTime StopTime;
+
+        private int _timerStartCode = 1;
+
+        public void Start()
+        {
+            try
+            {
+                if (_timerStartCode == -1)
+                {   
+
+                    throw new InvalidOperationException();
+                }
+                _timerStartCode = -1;
+                StartTime = DateTime.Now;
+
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Cannot start Stopwatch twice.");
+            }
+        }
+
+        public void Stop()
+        {
+            this.StopTime = DateTime.Now;
+        }
+
+        public TimeSpan ElapsedTime()
+        {
+            return StopTime - StartTime;
+
+        }
+
+    }
+}
